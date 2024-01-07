8c9c4e4d9b5ea3b57d25cdde2344a098bdccd251
thomasjshea <thomasjshea89@gmail.com>
2024-01-06 19:52:44 -0600

    updated app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.routes import home, dashboard
+from app.routes import home, dashboard, api
 from app.db import init_db
 
 def create_app(test_config=None):
@@ -17,6 +17,7 @@ def create_app(test_config=None):
     # register routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+    app.register_blueprint(api)
 
     init_db(app)
 
